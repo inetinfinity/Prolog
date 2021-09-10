b761f81535929f9284e4bc6c8344fe01a3fce9f4
Snow-Evil <inetinfinity@gmail.com>
2021-09-11 03:08:56 +0700

    add number functions

--- swj_assign2.pl
+++ swj_assign2.pl
@@ -23,3 +23,18 @@ rev(Xs,Ys,[X1|X2],[Y1|Y2],X,Y) :- rev(Xs,Ys,X2,Y2,[X1|X],[Y1|Y]).
 lastmem([],_).
 lastmem([X|Y],X1) :- Y = [],!,X1 = X.
 lastmem([_|Y],X) :- lastmem(Y,X).
+
+% Suwaroj-Assignment2: 3.1
+natural_number(0).
+natural_number(s(X)) :- natural_number(X).
+minus(X,0,X) :- natural_number(X).
+minus(s(X),s(Y),Z) :- minus(X,Y,Z).
+
+% Suwaroj-Assignment2: 3.2
+pl(0,X,X) :- natural_number(X).
+pl(s(X),Y,s(Z)) :- pl(X,Y,Z).
+times(0,X,0) :- natural_number(X).
+times(s(X),Y,Z) :- times(X,Y,W), pl(W,Y,Z).
+power(0,s(X),0) :- natural_number(X).
+power(s(X),0,s(0)) :- natural_number(X).
+power(X,s(W),Y) :- power(X,W,Z), times(Z,X,Y).
--- test.pl
+++ test.pl
@@ -55,3 +55,25 @@ rev(Xs,Ys,[X1|X2],[Y1|Y2],X,Y) :- rev(Xs,Ys,X2,Y2,[X1|X],[Y1|Y]).
 insert(X,[],[X]).
 insert(X,[Y|Ys],[Y|Zs]) :- X > Y,!,insert(X,Ys,Zs).
 insert(X,[Y|Ys],[X,Y|Zs]) :- X =< Y.
+
+natural_number(0).
+natural_number(s(X)) :- natural_number(X).
+
+pl(0,X,X) :- natural_number(X).
+pl(s(X),Y,s(Z)) :- pl(X,Y,Z).
+
+%minus(X,Y,Z).
+minus(X,0,X) :- natural_number(X).
+minus(s(X),s(Y),Z) :- minus(X,Y,Z).
+
+times(0,X,0).
+times(s(X),Y,Z) :- times(X,Y,W), pl(W,Y,Z).
+
+% power ok ver.1
+%power(s(X),0,0).
+%power(0,s(X),s(0)).
+%power(s(N),X,Y) :- power(N,X,Z),times(Z,X,Y).
+
+power(0,s(X),0).
+power(s(X),0,s(0)).
+power(X,s(W),Y) :- power(X,W,Z),times(Z,X,Y).
