660a66501037f19d0904e35758ebb52b6791c9d9
Snow-Evil <inetinfinity@gmail.com>
2021-09-17 03:38:01 +0700

    change 1

--- swj_assign2.pl
+++ swj_assign2.pl
@@ -1,18 +1,12 @@
 % Suwaroj-Assignment2: 1
 man(marcus).
-man(Y1) :- \+(ruler(Y1)),!.
-man(X4) :- \+(tryassassinate(X4,ceasar)),!.
-man(X4) :- \+(loyalto(X4,ceasar)).
 pompeian(marcus).
-pompeian(X1) :- roman(X1).
-ruler(ceasar).
-tryassassinate(marcus,ceasar).
-roman(X2) :- loyalto(X2,ceasar),!.
-roman(X2) :- hate(X2,ceasar).
-hate(X3,ceasar) :- \+(loyalto(X3,ceasar)).
-loyalto(X4) :- \+(man(X4)),!.
-loyalto(Y1) :- \+(ruler(Y1)),!.
-loyalto(X4,Y1) :- \+(tryassassinate(X4,Y1)).
+roman(X) :- pompeian(X).
+ruler(caesar).
+loyalto(X,caesar) :- roman(X).
+hate(X,caesar) :- roman(X).
+not(loyalto(X,Y)) :- man(X),ruler(Y),tryassassinate(X,Y).
+tryassassinate(marcus,caesar).
 
 % Suwaroj-Assignment2: 2.1
 rev(Xs,Ys) :- rev(Xs,Ys,Xs,Ys,[],[]).
@@ -38,17 +32,3 @@ times(s(X),Y,Z) :- times(X,Y,W), pl(W,Y,Z).
 power(0,s(X),0) :- natural_number(X).
 power(s(X),0,s(0)) :- natural_number(X).
 power(X,s(W),Y) :- power(X,W,Z), times(Z,X,Y).
-
-/*
-%%% SWJ-Test Case %%% (EXR. = Expected Result)
-1  : hate(marcus,ceasar).  // EXR.: true
-2.1: rev([a,b,c],Ys).  // EXR.: Ys = [c,b,a]
-2.1: rev([1,2,3,4,5],Ys).  // EXR.: Ys = [5,4,3,2,1]
-2.1: rev(Xs,[m,y,o,b]).  // EXR.: Xs = [b,o,y,m]
-2.2: lastmem([1,2,3],L).  // EXR.: L = 3
-2.2: lastmem([a,b,c,d],L).  // EXR.: L = d
-3.1: minus(s(s(s(s(0)))),s(s(s(0))),Z).  // EXR.: Z = s(0)
-3.1: minus(s(s(s(s(0)))),s(s(0)),Z).  // EXR.: Z = s(s(0))
-3.2: power(s(s(0)),s(s(s(0))),Z).  // EXR.: Z = s(s(s(s(s(s(s(s(0))))))))
-3.2: power(s(s(0)),0,Z).  // EXR.: Z = s(0)
-*/
